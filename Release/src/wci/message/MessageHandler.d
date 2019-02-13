1e54aed149a9f5b3740320d1cf343ad67164d551
songtoan0201 <songtoan0201@gmail.com>
2019-02-12 18:01:56 -0800

    delete files

--- Release/src/wci/message/MessageHandler.d
+++
@@ -1,9 +0,0 @@
-src/wci/message/MessageHandler.o: ../src/wci/message/MessageHandler.cpp \
- ../src/wci/message/MessageHandler.h ../src/wci/message/MessageListener.h \
- ../src/wci/message/Message.h
-
-../src/wci/message/MessageHandler.h:
-
-../src/wci/message/MessageListener.h:
-
-../src/wci/message/Message.h:
